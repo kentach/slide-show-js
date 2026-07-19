653a41300825f08e68506fc9eeca49da64f44726
Kenta <kenta@Kentas-MacBook-Pro.local>
2026-07-19 18:17:50 +0900

    fix:refacotring_into_setImages_function

--- index.js
+++ index.js
@@ -1,55 +1,56 @@
 {
-  function showImage(index) {
+  const images = [
+  {
+    src: "images/dog1.avif",
+    comment: "スマホばっか、いじるなよ〜",
+  },
+  {
+    src: "images/dog2.avif",
+    comment: "遊ぼうよ！",
+  },
+  {
+    src: "images/dog4.avif",
+    comment: "おやつまだ？",
+  },
+  {
+    src: "images/dog5.avif",
+    comment: "散歩に行きたい！",
+  },
+  {
+    src: "images/dog7.avif",
+    comment: "今日も元気！",
+  },
+  {
+    src: "images/dog8.avif",
+    comment: "眠くなってきた…",
+  },
+  {
+    src: "images/dog9.jpg",
+    comment: "なでてほしいな🐶",
+  },
+];
+
+  const thumbnailContainer = document.querySelector('.thumbnails-container');
+  const mainImage = document.querySelector('.main-image');
+  const prevBtn = document.getElementById('prev-button');
+  const nextBtn = document.getElementById('next-button');
+  let currentIndex = 0;
+
+  function setImages(index) {
+    currentIndex = index;
+
+    const comment = document.querySelector('.comment');
+    mainImage.src = images[index].src; // メイン画像のセット
+    comment.textContent = images[index].comment; // コメントのセット
+
     thumbnails.forEach((thumb) => {
       thumb.classList.remove("active");
     });
-  
-    currentIndex = index;
 
-    mainImage.classList.remove("fade");
-  
-    // アニメーションを再実行
-    void mainImage.offsetWidth;
-  
-    mainImage.src = thumbnails[currentIndex].src;
-    thumbnails[currentIndex].classList.add("active");
-    mainImage.classList.add("fade");
+    thumbnails[index].classList.add("active");
   }
 
-
-  const images = [
-    {
-      src: "images/dog1.avif",
-      comment: "スマホばっか、いじるなよ〜",
-    },
-    {
-      src: "images/dog2.avif",
-      comment: "遊ぼうよ！",
-    },
-    {
-      src: "images/dog4.avif",
-      comment: "おやつまだ？",
-    },
-    {
-      src: "images/dog5.avif",
-      comment: "散歩に行きたい！",
-    },
-    {
-      src: "images/dog7.avif",
-      comment: "今日も元気！",
-    },
-    {
-      src: "images/dog8.avif",
-      comment: "眠くなってきた…",
-    },
-    {
-      src: "images/dog9.jpg",
-      comment: "なでてほしいな🐶",
-    },
-  ];
-
-  const thumbnailContainer = document.querySelector('.thumbnails-container');
-  
+  // 画像の配列の展開
   images.forEach((image) => { 
     const thumbImage = document.createElement('img');
     thumbImage.src = image.src;
@@ -57,57 +58,36 @@
     thumbnailContainer.appendChild(thumbImage);
   });
 
-  const mainImage = document.querySelector('.main-image');
-  const prevBtn = document.getElementById('prev-button');
-  const nextBtn = document.getElementById('next-button');
   const thumbnails = document.querySelectorAll('.thumbnail');
   thumbnails[0].classList.add('active');
+  
 
-  let currentIndex = 0;
-
+  // サムネイル画像がクリックされたら
   thumbnails.forEach((thumbnail, index) => {
     thumbnail.addEventListener('click', () => {
-      showImage(index);
-
-      thumbnails.forEach((thumb) => {
-        thumb.classList.remove('active')
-      });
-
-      currentIndex = index;
-
-      mainImage.src = thumbnail.src;
-      thumbnail.classList.add('active');
+      setImages(index);
     })
   })
 
-
+  // 次ボタン
   nextBtn.addEventListener('click', () => {
-    thumbnails.forEach((thumb) => {
-      thumb.classList.remove('active')
-    });
-
     currentIndex++;
+
     if (currentIndex > thumbnails.length - 1) {
       currentIndex = 0;
     } 
-    mainImage.src = thumbnails[currentIndex].src;
-    thumbnails[currentIndex].classList.add('active');
-    
+    setImages(currentIndex);
   })
 
 
   prevBtn.addEventListener('click', () => {
-    thumbnails.forEach((thumb) => {
-      thumb.classList.remove('active')
-    });
-    
-    if (currentIndex === 0) {
-      currentIndex += thumbnails.length;
-    }
     currentIndex--;
-
-    mainImage.src = thumbnails[currentIndex].src;
-    thumbnails[currentIndex].classList.add('active');
+    
+    if (currentIndex < 0) {
+      currentIndex = thumbnails.length - 1;
+    }
+    
+    setImages(currentIndex);
   })
 
 }
